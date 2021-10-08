d5f40f71ede4c3462f74df65f3393ba678003356
Denis Smirnov <sd@arenadata.io>
2021-10-08 21:04:52 +0300

    Use 63k packets for tests

--- src/ipc.h
+++ src/ipc.h
@@ -1,8 +1,10 @@
 #ifndef IPC_H
 #define IPC_H
 
-#define PACKET_SIZE 65 * 1024
+#define PACKET_SIZE 63 * 1024
 #define SOCKET_PATH "/tmp/.unix.sock"
+#define SOCKET_ADDR "127.0.0.1"
+#define SOCKET_PORT 8888
 
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while(0)
