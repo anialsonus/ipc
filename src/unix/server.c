fb6b44e9cf703a71818726ae4103f18eaf8e46fd
Denis Smirnov <sd@arenadata.io>
2021-10-11 18:51:01 +0000

    Implement critical sections and non-blocking read
    Done for Unix server in the current commit.

--- src/unix/server.c
+++ src/unix/server.c
@@ -1,30 +1,45 @@
-#include <poll.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <poll.h>
 #include <unistd.h>
 
 #include "../ipc.h"
 
 #define RCV_BUFFER_SIZE 512 * 1024 - 1
 
-/* Non-atomic, but it is ok for our test case */
 static volatile uint64_t totalsize;
+
+/* Signum of the pending signal */
+static volatile sig_atomic_t signal_pending = 0;
+/* Do we have a pending signal? */
+static volatile sig_atomic_t defer_signal = 0;
 
 void termination_handler(int signum)
 {
-    printf("Total bytes: %llu\n", totalsize);
-    exit(EXIT_SUCCESS);
+    if (defer_signal)
+    {
+        signal_pending = signum;
+    }
+    else
+    {
+        printf("Total bytes: %" PRIu64 "\n", totalsize);
+        exit(EXIT_SUCCESS);
+    }
 }
 
 int
 main (int argc, char **argv)
 {
     int sockbuf;
+    int flags;
     char readbuf[PACKET_SIZE];
     int res;
     int sockfd;
@@ -39,6 +54,13 @@ main (int argc, char **argv)
     sockfd = socket(AF_LOCAL, SOCK_DGRAM, 0);
     if (sockfd < 0)
         handle_error("Failed to create a Unix socket");
+
+    /* Make socket non-blocking */
+    flags = fcntl(sockfd, F_GETFL, 0);
+    if(flags < 0)
+        handle_error("Failed to get socket descriptor flags");
+    if(fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) < 0)
+        handle_error("Failed to add non-blocking flag to the socket");
 
     /* Set socket receive buffer size */
     sockbuf = RCV_BUFFER_SIZE;
@@ -76,18 +98,29 @@ main (int argc, char **argv)
         
         if(pfds[0].revents & POLLIN)
         {
-            size = recvfrom(pfds[0].fd, &readbuf, sizeof(readbuf), 0, NULL, NULL);
-            if (size < 0)
-                perror("Failed to receive from the socket");
-            else
+            while ((size = recvfrom(pfds[0].fd, &readbuf, sizeof(readbuf), 0, NULL, NULL)) >= 0)
+            {
+                /* Block SIGINT while update critical section */
+                defer_signal++;
+
+                /* Update counter */
                 totalsize += size;
+
+                /* Unblock critical section and reraise SIGINT */
+                defer_signal--;
+                if (!defer_signal && signal_pending != 0)
+                    raise(signal_pending);
+
+            }
+            if (errno != EAGAIN)
+                perror("Failed to receive from the socket");
 #ifdef DEBUG
             printf("Received %lu bytes from the socket\n", size);
 #endif
         }
         else
         {
-            printf("Total bytes: %llu\n", totalsize);
+            printf("Total bytes: %" PRIu64 "\n", totalsize);
             if (close(pfds[0].fd) < 0)
                 handle_error("Failed to close socket");
             if ((access(SOCKET_PATH, R_OK) == 0) && unlink(SOCKET_PATH) < 0)
